4faacf2a31b8c700f4fec65f59b06bb9ba5c7e58
FudgyDRS <yetimonsters@gmail.com>
2023-12-01 06:46:51 +0100

    supporting files

--- script/deploy.s.sol
+++ script/deploy.s.sol
@@ -3,6 +3,8 @@ pragma solidity ^0.8.15;
 
 import "../test/base/loadkey.t.sol";
 import "forge-std/console.sol";
+import {IEntryPoint, EntryPoint, IAccount, UserOperation, UserOperationLib} from "@4337/core/entryPoint.sol";
+import {SimpleAccount, SimpleAccountFactory} from "@4337/samples/SimpleAccountFactory.sol";
 
 contract Deploy is LoadKey {
     // using UserOperationLib for UserOperation;
@@ -13,7 +15,20 @@ contract Deploy is LoadKey {
 
     function run() public {
         vm.startBroadcast(privateKey);
+        //
+        IEntryPoint _entryPoint;
+        address entryPointAddress;
+        //vm.nonce(72);
+        SimpleAccountFactory _simpleAccountFactory;
+        address simpleAccountFactoryAddress;
+        //vm.nonce(73);
+        _entryPoint = new EntryPoint();
+        entryPointAddress = address(_entryPoint);
+
+        _simpleAccountFactory = new SimpleAccountFactory(
+            IEntryPoint(entryPointAddress)
+        );
+        simpleAccountFactoryAddress = address(_simpleAccountFactory);
         vm.stopBroadcast();
     }
-
 }
